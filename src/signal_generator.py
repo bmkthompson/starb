8ddf87b5d0add346c5be17b6b0331ded769800fd
bmkthompson <151477884+bmkthompson@users.noreply.github.com>
2025-06-18 23:02:04 -0700

    Update signal_generator.py

--- src/signal_generator.py
+++ src/signal_generator.py
@@ -1,22 +1,32 @@
+import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 
-def calculate_spread(series1, series2, beta):
-    df = pd.concat([series1, series2], axis=1).dropna()
-    spread = df.iloc[:, 0] - beta * df.iloc[:, 1]
-    return spread
+def calculate_rolling_beta(series1, series2, window):
+    betas = []
+    for i in range(len(series1)):
+        if i < window:
+            betas.append(np.nan)
+            continue
+        y_window = series1.iloc[i-window:i]
+        x_window = series2.iloc[i-window:i]
+        x_with_const = sm.add_constant(x_window)
+        model = sm.OLS(y_window, x_with_const).fit()
+        beta = model.params[1]
+        betas.append(beta)
+    return pd.Series(betas, index=series1.index)
 
-def calculate_zscore(spread, window=30):
-    mean = spread.rolling(window).mean()
-    std = spread.rolling(window).std()
-    zscore = (spread - mean) / std
-    return zscore
+def generate_signals_rolling_beta(series1, series2, window=60, z_entry=1.0, z_exit=0.5):
+    betas = calculate_rolling_beta(series1, series2, window)
+    spread = series1 - betas * series2
 
-def generate_signals(zscore, entry_threshold=1.0, exit_threshold=0.25):
-    signals = pd.Series(index=zscore.index, dtype='float64')
-    
-    signals[zscore > entry_threshold] = -1  # Short spread
-    signals[zscore < -entry_threshold] = 1  # Long spread
-    signals[abs(zscore) < exit_threshold] = 0  # Exit position
+    spread_mean = spread.rolling(window).mean()
+    spread_std = spread.rolling(window).std()
+    zscore = (spread - spread_mean) / spread_std
 
-    signals.ffill(inplace=True)
-    return signals
+    signals = pd.Series(0, index=series1.index)
+    signals[zscore > z_entry] = -1
+    signals[zscore < -z_entry] = 1
+    signals[(zscore < z_exit) & (zscore > -z_exit)] = 0
+
+    return signals, spread, zscore, betas
